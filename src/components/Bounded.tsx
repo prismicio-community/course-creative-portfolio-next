f70cfba0bb1054a5a55379d519bf670203baa750
Alex Trost <19617280+a-trost@users.noreply.github.com>
2023-12-04 09:19:14 -0500

    fix: Add forwardRef to bounded

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,26 +1,27 @@
+import React from "react";
 import clsx from "clsx";
 
 type BoundedProps = {
   as?: React.ElementType;
   className?: string;
-  ref?: React.Ref<HTMLDivElement>;
   children: React.ReactNode;
 };
 
-export default function Bounded({
-  as: Comp = "section",
-  className,
-  children,
-  ref,
-  ...restProps
-}: BoundedProps) {
-  return (
-    <Comp
-      ref={ref}
-      className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
-      {...restProps}
-    >
-      <div className="mx-auto w-full max-w-7xl">{children}</div>
-    </Comp>
-  );
-}
+const Bounded = React.forwardRef<HTMLDivElement, BoundedProps>(
+  ({ as: Comp = "section", className, children, ...restProps }, ref) => {
+    return (
+      <Comp
+        ref={ref}
+        className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
+        {...restProps}
+      >
+        <div className="mx-auto w-full max-w-7xl">{children}</div>
+      </Comp>
+    );
+  },
+);
+
+// Set a display name for the component
+Bounded.displayName = "Bounded";
+
+export default Bounded;
